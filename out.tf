19ddb6e41e7e58dd3356fcde2ce4699c4f23fa91
persiidan <136482383+persiidan@users.noreply.github.com>
2025-05-19 23:00:22 +0300

    Update out.tf

--- out.tf
+++ out.tf
@@ -1,4 +1,4 @@
-output "loadbalancer" {
+output "alb_dns" {
   value = module.load_balancer.ALB_DNS
   description = "the ALB dns name for ease-of-use (see it in the cli no need to check in web)"
 }
