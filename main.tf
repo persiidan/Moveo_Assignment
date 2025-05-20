58b5d87df6a2ab635627f4293d6d972ec6611f83
Idan Persi <persiidan@gmail.com>
2025-05-20 20:31:46 +0300

    fixed?

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24"]
   public_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
   manage_default_security_group = false
+  manage_default_network_acl = false
   create_igw = true
   enable_nat_gateway = true
   single_nat_gateway = true 
