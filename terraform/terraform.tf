1a255c74f987d66a51bfae2bfcc30a8d258cf20a
Idan Persi <persiidan@gmail.com>
2025-10-15 16:00:57 +0300

    added s3 backend, tf files now in 'terraform' folder, updated deploy workflow

---
+++ terraform/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+      }
+  }
+
+  backend "s3" {
+    bucket         = "moveoterrabe"
+    key            = "terraform.tfstate"
+    region         = "il-central-1"
+    }
+    required_version = "~> 1.12.0"
+}
+provider "aws" {
+  region = "il-central-1"
+}
